b9df391d8a73ccca6471cf76552bac51829cf3a5
MacBookPro15Zoll <ludwig.leuschner@datev.de>
2017-04-20 09:46:43 +0200

    Integer commit

---
+++ app/src/main/java/com/example/teamrocket/tictactoe/MainActivity.java
@@ -0,0 +1,254 @@
+package com.example.teamrocket.tictactoe;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int player=0;
+    private int counter=0;
+    private int symbolPlayer1=R.drawable.rsz_tic_tac_toe_x_white;
+    private int symbolPlayer2=R.drawable.rsz_tic_tac_toe_o_white;
+
+    private int[][] field = {
+            {-1,-1,-1},
+            {-1,-1,-1},
+            {-1,-1,-1}
+    };
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    ImageView playerSymbol;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpLayout();
+
+        Intent intent = getIntent();
+        int symbol1 = intent.getIntExtra("symbol1", R.drawable.rsz_tic_tac_toe_x);
+        int symbol2 = intent.getIntExtra("symbol2", R.drawable.rsz_tic_tac_toe_o);
+
+        switch (symbol1) {
+            case R.drawable.rsz_tic_tac_toe_x:
+                symbolPlayer1 = R.drawable.rsz_tic_tac_toe_x_white;
+                break;
+            case R.drawable.rsz_tic_tac_toe_o:
+                symbolPlayer1 = R.drawable.rsz_tic_tac_toe_o_white;
+                break;
+            case R.drawable.rsz_tic_tac_toe_star:
+                symbolPlayer1 = R.drawable.rsz_tic_tac_toe_star_white;
+                break;
+            default:
+                symbolPlayer1 = R.drawable.rsz_tic_tac_toe_x_white;
+        }
+
+        switch (symbol2) {
+            case R.drawable.rsz_tic_tac_toe_x:
+                symbolPlayer2 = R.drawable.rsz_tic_tac_toe_x_white;
+                break;
+            case R.drawable.rsz_tic_tac_toe_o:
+                symbolPlayer2 = R.drawable.rsz_tic_tac_toe_o_white;
+                break;
+            case R.drawable.rsz_tic_tac_toe_star:
+                symbolPlayer2 = R.drawable.rsz_tic_tac_toe_star_white;
+                break;
+            default:
+                symbolPlayer2 = R.drawable.rsz_tic_tac_toe_o_white;
+        }
+    }
+
+    private void setUpLayout(){
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        playerSymbol = (ImageView) findViewById(R.id.playerSymbol);
+
+        playerSymbol.setImageDrawable(getDrawable(symbolPlayer1));
+
+        View.OnClickListener buttonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button thisButton = (Button) v;
+                int tag = Integer.parseInt(thisButton.getTag().toString());
+                int zeile = tag/3;
+                int spalte = tag%3;
+                Log.i("####","Background: " + thisButton.getBackground().toString());
+                if (thisButton.getText()=="") {
+                    field[zeile][spalte]=player;
+                    if (player == 0) {
+                        thisButton.setBackground(getDrawable(symbolPlayer1));
+                        playerSymbol.setImageDrawable(getDrawable(symbolPlayer2));
+                        player = 1;
+                    } else {
+                        thisButton.setBackground(getDrawable(symbolPlayer2));
+                        playerSymbol.setImageDrawable(getDrawable(symbolPlayer1));
+                        player = 0;
+                    }
+                    thisButton.setText("set");
+                    counter++;
+                }
+
+                String ausgabe="";
+                for(int i=0; i<3; i++) {
+                    for (int j=0; j<3; j++) {
+                        ausgabe+=field[i][j];
+                    }
+                    ausgabe+="\n";
+                }
+                Log.i("####", ausgabe);
+
+                checkEndGame();
+            }
+        };
+
+        button1.setOnClickListener(buttonListener);
+        button2.setOnClickListener(buttonListener);
+        button3.setOnClickListener(buttonListener);
+        button4.setOnClickListener(buttonListener);
+        button5.setOnClickListener(buttonListener);
+        button6.setOnClickListener(buttonListener);
+        button7.setOnClickListener(buttonListener);
+        button8.setOnClickListener(buttonListener);
+        button9.setOnClickListener(buttonListener);
+    }
+
+    private void checkEndGame() {
+        int gewinnerHorizontal = checkHorizontal();
+        int gewinnerVertical = checkVertical();
+        int gewinnerDiagonal = checkDiagonal();
+        if(gewinnerHorizontal != -1){
+            gewinnerHorizontal++;
+            startAlert("Spieler " + gewinnerHorizontal + " hat gewonnen!");
+        } else if(gewinnerVertical != -1){
+            gewinnerVertical++;
+            startAlert("Spieler " + gewinnerVertical + " hat gewonnen!");
+        } else if(gewinnerDiagonal != -1){
+            gewinnerDiagonal++;
+            startAlert("Spieler " + gewinnerDiagonal + " hat gewonnen!");
+        } else if(counter==9) {
+            startAlert("Unentschieden!");
+        }
+    }
+
+    private void startAlert(String message){
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Spiel Beendet")
+                .setMessage(message+"\nWillst du nochmal spielen?")
+                .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        cleanGame();
+                    }
+                })
+                .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .show();
+    }
+
+    private int checkDiagonal() {
+        if(field[0][0] == field[1][1] && field[0][0] == field[2][2] && field[0][0] !=-1) {
+            Log.i("####","check Diagonal 1");
+            return field[0][0];
+        }else if(field[2][0] == field[1][1] && field[2][0] == field[0][2] && field[2][0] !=-1) {
+            Log.i("####","check Diagonal 2");
+            return field[2][0];
+        }
+        return -1;
+    }
+
+    private int checkVertical() {
+        if(field[0][0] == field[1][0] && field[0][0] == field[2][0] && field[0][0] !=-1) {
+            Log.i("####","check Vertical 1");
+            return field[0][0];
+        }else if(field[0][1] == field[1][1] && field[0][1] == field[2][1] && field[0][1] !=-1) {
+            Log.i("####","check Vertical 2");
+            return field[0][1];
+        }else if(field[0][2] == field[1][2] && field[0][2] == field[2][2] && field[0][2] !=-1) {
+            Log.i("####","check Vertical 3");
+            return field[0][2];
+        }
+        return -1;
+    }
+
+    private int checkHorizontal() {
+        if(field[0][0] == field[0][1] && field[0][0] == field[0][2] && field[0][0] !=-1) {
+            Log.i("####","check Horizontal 1");
+            return field[0][0];
+        }else if(field[1][0] == field[1][1] && field[1][0] == field[1][2]&& field[1][0] !=-1) {
+            Log.i("####","check Horizontal 2");
+            return field[1][0];
+        }else if(field[2][0] == field[2][1] && field[2][0] == field[2][2] && field[2][0] !=-1) {
+           Log.i("####","check Horizontal 3");
+            return field[2][0];
+        }
+        return -1;
+    }
+
+    public void cleanGame() {
+        player = 0;
+        counter = 0;
+
+
+        button1.setBackground(getDrawable(R.color.transparent));
+        button2.setBackground(getDrawable(R.color.transparent));
+        button3.setBackground(getDrawable(R.color.transparent));
+        button4.setBackground(getDrawable(R.color.transparent));
+        button5.setBackground(getDrawable(R.color.transparent));
+        button6.setBackground(getDrawable(R.color.transparent));
+        button7.setBackground(getDrawable(R.color.transparent));
+        button8.setBackground(getDrawable(R.color.transparent));
+        button9.setBackground(getDrawable(R.color.transparent));
+
+        button1.setText("");
+        button2.setText("");
+        button3.setText("");
+        button4.setText("");
+        button5.setText("");
+        button6.setText("");
+        button7.setText("");
+        button8.setText("");
+        button9.setText("");
+        playerSymbol.setImageDrawable(getDrawable(symbolPlayer1));
+
+        for(int i=0; i<=2; i++) {
+            for(int j=0; j<=2; j++) {
+                field[i][j] = -1;
+            }
+        }
+    }
+
+    public void setSymbolPlayer1(int symbol) {
+        symbolPlayer1=symbol;
+    }
+
+    public void setSymbolPlayer2(int symbol) {
+        symbolPlayer2=symbol;
+    }
+
+}
---
+++ app/src/main/java/com/example/teamrocket/tictactoe/MenuActivity.java
@@ -0,0 +1,58 @@
+package com.example.teamrocket.tictactoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    SimpleSpinnerAdapter adapter1, adapter2;
+    Spinner firstPlayer, secondPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        firstPlayer = (Spinner)findViewById(R.id.spinner1);
+        Integer[] items = new Integer[]{R.drawable.rsz_tic_tac_toe_o,R.drawable.rsz_tic_tac_toe_x,R.drawable.rsz_tic_tac_toe_star};
+        adapter1 = new SimpleSpinnerAdapter(this,items);
+        firstPlayer.setAdapter(adapter1);
+
+        secondPlayer = (Spinner)findViewById(R.id.spinner2);
+        adapter2 = new SimpleSpinnerAdapter(this,items);
+        adapter2.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        secondPlayer.setAdapter(adapter2);
+
+        ImageButton startButton= (ImageButton) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+    }
+
+    private void startGame() {
+        int selectedItemPlayer1 = (int) firstPlayer.getItemAtPosition(firstPlayer.getSelectedItemPosition());
+        int selectedItemPlayer2 = (int) secondPlayer.getItemAtPosition(secondPlayer.getSelectedItemPosition());
+        if(selectedItemPlayer1 == selectedItemPlayer2) {
+            Toast toast = Toast.makeText(this, "Es müssen unterschiedliche Symbole ausgewählt werden!", Toast.LENGTH_LONG);
+            toast.show();
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("symbol1", selectedItemPlayer1);
+            intent.putExtra("symbol2", selectedItemPlayer2);
+            startActivity(intent);
+        }
+
+    }
+
+
+}
